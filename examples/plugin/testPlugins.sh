f5cecda590f7a01bb93cc9b2dfb055d99287abc5
Jerome Brette <jbrette@gmail.com>
2019-09-15 18:17:02 +0000

    Update transformer config in each example

--- examples/plugin/testPlugins.sh
+++ examples/plugin/testPlugins.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+export BASEDIR=.
+
 set -e
 
 echo Testing built-in plugins...
 
-for i in `cat examples/plugin/builtInPluginList | grep -v '#'`
+for i in `cat ${BASEDIR}/builtInPluginList | grep -v '#'`
 do
  echo $i
- mkdir -p examples/plugin/$i/expected
- kustomize build examples/plugin/$i -o examples/plugin/${i}/expected/result.yaml
+ mkdir -p ${BASEDIR}/$i/expected
+ kustomize build ${BASEDIR}/$i -o ${BASEDIR}/${i}/expected/result.yaml
 done
 
 echo Testing external plugins...
 
-for i in `cat examples/plugin/externalPluginList | grep -v '#'`
+for i in `cat ${BASEDIR}/externalPluginList | grep -v '#'`
 do
  echo $i
- mkdir -p examples/plugin/$i/expected
- kustomize build examples/plugin/$i -o examples/plugin/${i}/expected/result.yaml --enable_alpha_plugins
+ mkdir -p ${BASEDIR}/$i/expected
+ kustomize build ${BASEDIR}/$i -o ${BASEDIR}/${i}/expected/result.yaml --enable_alpha_plugins
 done
 
 echo All done.
